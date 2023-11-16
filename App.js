b1523538c8dd7d3cf880f8826dcc06c6b672f5b4
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-11-16 18:25:53 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,12 +3,24 @@ import React from 'react'
 function App() {
     const students = ['Rahul','Garun','Devesh','Raja'];
     //map looping
-    students.map((item) => {
-        console.warn("My name is:", item)
-    });
+   // students.map((item) => {
+   //     console.warn("My name is:", item)
+ //   });
+
+    //display names using for loop
+
+  //  for(let i=0; i<students.length;i++)
+   // {
+  //      console.warn("in for loop My name is:",student[i])
+  //  }
     return (
         <div className="App">
             <h1>Handling array using List</h1>
+            {
+              students.map((data)=>
+              <h1>[data]</h1>
+            )  
+            }
         </div>
     );
 }
